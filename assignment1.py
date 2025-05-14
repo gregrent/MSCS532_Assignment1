c42a21e50efeab44c8c037119468115757f868db
gregrent <gvillar39058@ucumberlands.edu>
2025-05-14 14:33:50 -0500

    Added insertionSort function with description comments, and temporary test call

--- assignment1.py
+++ assignment1.py
@@ -4,3 +4,13 @@
 # Insertion sort algorithm that sorts
 # in monotonically decreasing order.
 #########################################
+
+#  A (list): The list of elements to be sorted.
+#  n (int): The number of elements in the list A.
+def insertionSort(A, n):
+   print ("insertionSort called") # placeholder for now
+
+
+# Temporary test call
+A = [73, 8, 42, 19, 91, 27, 5, 64, 33, 50]
+insertionSort(A, len(A))
